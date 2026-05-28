e2235b8fd77359c458aa3d417c5346821e89a9bd
Ruben <meyer.nano@gmail.com>
2026-05-28 11:56:04 +0200

    Point live viewer to selected four-cell halo model

--- interactive-3d-solar-panel/src/SolarPanelScene.js
+++ interactive-3d-solar-panel/src/SolarPanelScene.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/nanotech-solutions-norway/animations-SolarEX_1.0@baad37680da03f64fd7aa9d66ba0a22682be0c27/interactive-3d-solar-panel/src/SolarPanelScene.js';
+import 'https://cdn.jsdelivr.net/gh/nanotech-solutions-norway/animations-SolarEX_1.0@e6729ea211d85369fa033cc09f2c1536d87f401d/interactive-3d-solar-panel/src/SolarPanelScene.js';
