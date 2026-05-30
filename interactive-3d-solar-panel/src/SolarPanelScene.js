4e528c274c27a19186bf07c28de9249162a73d73
Ruben <meyer.nano@gmail.com>
2026-05-30 11:31:59 +0200

    Point live viewer to responsive mobile model

--- interactive-3d-solar-panel/src/SolarPanelScene.js
+++ interactive-3d-solar-panel/src/SolarPanelScene.js
@@ -1,1 +1,1 @@
-import 'https://cdn.jsdelivr.net/gh/nanotech-solutions-norway/animations-SolarEX_1.0@e6729ea211d85369fa033cc09f2c1536d87f401d/interactive-3d-solar-panel/src/SolarPanelScene.js';
+import 'https://cdn.jsdelivr.net/gh/nanotech-solutions-norway/animations-SolarEX_1.0@8af338f577e23a7a2fe8f3df31bd6823eac90a22/interactive-3d-solar-panel/src/SolarPanelScene.js';
